f51c9c633f6fed2a93495fc495ff3894ca1da647
takofaru <fadelior@gmail.com>
2025-05-21 22:18:30 +0700

    add a mode

--- scripts/pemula.jsx
+++ scripts/pemula.jsx
@@ -14,23 +14,48 @@ function Counter() {
     return(
         <>
             <div className="counter">
-                <button onClick={() => handleCount('add')}>+</button>
-                <p>{count}</p>
-                <button onClick={() => handleCount('')}>-</button>
+                <div className="main">
+                    <button onClick={() => handleCount('add')}>+1</button>
+                    <p>{count}</p>
+                    <button onClick={() => handleCount('')}>-1</button>
+                </div>
+                <button onClick={() => resetCounter()}>Reset</button>
             </div>
-            <button onClick={() => resetCounter()}>Reset</button>
         </>
     );
 }
 
 function AnotherCounter() {
+    const [count, setCount] = React.useState(1);
+
+    function handleCount(mode) {
+        setCount(count => mode === 'times' ? count * 10 : count / 10);
+    }
     
+    function resetCounter() {
+        setCount(1);
+    }
+    return(
+        <>
+            <div className="counter">
+                <div className="main">
+                    <button onClick={() => handleCount('times')}>*10</button>
+                    <p>{count}</p>
+                    <button onClick={() => handleCount('')}>/10</button>
+                </div>
+                <button onClick={() => resetCounter()}>Reset</button>
+            </div>
+        </>
+    );
 }
 
 function HomePage() {
+    const [showAnother, setAnother] = React.useState(false);
+
     return (
         <div>
-            <Counter />
+            <button onClick={() => setAnother(prev => !prev)}>Set mode</button>
+            {showAnother ? <AnotherCounter/> : <Counter/>}
             {/* <AnotherCounter /> */}
         </div>
     );
